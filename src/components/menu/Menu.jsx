50a1ae669746d3130267d28e9e66d8a2d0383b65
aayush <aausgh@gmail.com>
2024-04-04 19:44:40 +0545

    fix

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -45,7 +45,7 @@ const Menu = () => {
 
     return (
         <div className={styles.container}>
-            <h2 className={styles.subtitle}>What's hot</h2>
+            <h2 className={styles.subtitle}>What&apos; hot</h2>
             <h1 className={styles.title}>Most Popular</h1>
 
             <div className={styles.items}>
